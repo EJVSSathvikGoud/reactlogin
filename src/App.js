f6b6ec81810d93de892ae53ff37cb4c996ff2f76
EJVSSathvikGoud <sathivkgoud123@gmail.com>
2021-09-27 11:14:15 +0530

    dfdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
+// import { response } from 'express';
 
 function App() {
+  const responseGoogle = (response) =>{
+    console.log(response)
+    return console.log("somring ")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello eor
+      <GoogleLogin
+    clientId="801125525880-ol7ifi38t43diqavdcmatvu3n0cfjfua.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
     </div>
   );
 }
